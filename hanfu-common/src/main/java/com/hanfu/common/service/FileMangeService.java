a043176c3040ff30765f9b184d25fd6105fd3019
davin <davinzhang@outlook.com>
2019-09-24 15:27:06 +0800

    bug fix for master

---
+++ hanfu-common/src/main/java/com/hanfu/common/service/FileMangeService.java
@@ -0,0 +1,11 @@
+package com.hanfu.common.service;
+
+import org.springframework.stereotype.Service;
+
+//@Service
+public class FileMangeService {
+	
+	public void getFile() {
+		System.out.println("ssssssssssss");
+	}
+}
