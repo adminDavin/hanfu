fc6ac4a9a1d82919f465fcfd19f2afb7cab92f20
wangbopeng1 <wangbo_peng@163.com>
2019-11-11 17:31:15 +0800

    更新

--- hanfu-referral-center/src/main/java/com/hanfu/referral/center/controller/HfUserAddressController.java
+++ hanfu-referral-center/src/main/java/com/hanfu/referral/center/controller/HfUserAddressController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hanfu.referral.center.service.ReferralUserAddressService;
 import com.hanfu.referral.center.service.ReferralUserService;
 import com.hanfu.utils.response.handler.ResponseEntity;
 import com.hanfu.utils.response.handler.ResponseUtils;
@@ -17,14 +18,14 @@ import com.hanfu.utils.response.handler.ResponseEntity.BodyBuilder;
 import io.swagger.annotations.Api;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/address")
 @Api
-public class HfUserController {
+public class HfUserAddressController {
 	@Autowired
-	private ReferralUserService referralUserService;
+	private ReferralUserAddressService referralUserAddressService;
 	@RequestMapping(path = "/findAllUser",method = RequestMethod.GET)
 	public ResponseEntity<Map<String, Object>> findAllOrder(Integer id) throws JSONException {
         BodyBuilder builder = ResponseUtils.getBodyBuilder(HttpStatus.OK);
-        return builder.body(ResponseUtils.getResponseBody(referralUserService.getUserById(id)));
+        return builder.body(ResponseUtils.getResponseBody(referralUserAddressService.getUserAddressById(id)));
     }
 }
