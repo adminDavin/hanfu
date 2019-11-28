f6772e90ae389abbfc6f24b2b743d79c37c4a977
sunwangda <2451203734@qq.com>
2019-11-28 18:07:54 +0800

    1

--- hanfu-activity-center/src/main/java/com/hanfu/activity/center/manual/dao/HfUserDaoImpl.java
+++ hanfu-activity-center/src/main/java/com/hanfu/activity/center/manual/dao/HfUserDaoImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.hanfu.activity.center.manual.model.HfUser;
+import com.hanfu.activity.center.manual.model.UserFormInfo;
 
 @Repository
 public class HfUserDaoImpl implements HfUserDao{
@@ -17,6 +18,12 @@ public class HfUserDaoImpl implements HfUserDao{
 	@Override
 	public List<HfUser> findAllUser() {
 		List<HfUser> result = sqlSessionTemplate.selectList("findAllUser");
+		return result;
+	}
+	
+	@Override
+	public UserFormInfo findByUserId(Integer userId) {
+		UserFormInfo result = sqlSessionTemplate.selectOne("findByUserId", userId);
 		return result;
 	}
 	
---
+++ hanfu-activity-center/src/main/java/com/hanfu/activity/center/manual/model/UserFormInfo.java
@@ -0,0 +1,100 @@
+package com.hanfu.activity.center.manual.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class UserFormInfo implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3487964551024846418L;
+	
+	private Integer fileId;
+	private String username;
+	private String departmentName;
+	private Date hiredate;
+	private String jobContent;
+	private String phone;
+	private String evaluation;
+	private Integer departmentId;
+	private String A;
+	private String B;
+	private String C;
+	private String D;
+	public Integer getFileId() {
+		return fileId;
+	}
+	public void setFileId(Integer fileId) {
+		this.fileId = fileId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getDepartmentName() {
+		return departmentName;
+	}
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+	public Date getHiredate() {
+		return hiredate;
+	}
+	public void setHiredate(Date hiredate) {
+		this.hiredate = hiredate;
+	}
+	public String getJobContent() {
+		return jobContent;
+	}
+	public void setJobContent(String jobContent) {
+		this.jobContent = jobContent;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEvaluation() {
+		return evaluation;
+	}
+	public void setEvaluation(String evaluation) {
+		this.evaluation = evaluation;
+	}
+	public Integer getDepartmentId() {
+		return departmentId;
+	}
+	public void setDepartmentId(Integer departmentId) {
+		this.departmentId = departmentId;
+	}
+	public String getA() {
+		return A;
+	}
+	public void setA(String a) {
+		A = a;
+	}
+	public String getB() {
+		return B;
+	}
+	public void setB(String b) {
+		B = b;
+	}
+	public String getC() {
+		return C;
+	}
+	public void setC(String c) {
+		C = c;
+	}
+	public String getD() {
+		return D;
+	}
+	public void setD(String d) {
+		D = d;
+	}
+	
+	
+}
