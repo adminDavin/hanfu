d5aedecf7f2ddb50ee4c97ef721af0e30c2c3683
shihaoluoling <441168597@qq.com>
2019-12-24 17:49:21 +0800

    //会员功能重新写了   因为把会员表拉出来了

--- hanfu-zslpayment-center/src/main/java/com/hanfu/payment/center/entity/OrderInfo.java
+++ hanfu-zslpayment-center/src/main/java/com/hanfu/payment/center/entity/OrderInfo.java
@@ -22,8 +22,6 @@ public class OrderInfo {
     private String trade_type;// 交易类型
     private String openid;//用户标识
 
-    private int order_id;
-
     public String getAppid() {
         return appid;
     }
@@ -118,12 +116,5 @@ public class OrderInfo {
 
     public void setOpenid(String openid) {
         this.openid = openid;
-    }
-
-    public int getOrder_id() {
-        return order_id;
-    }
-    public void setOrder_id(int order_id) {
-        this.order_id = order_id;
     }
 }
