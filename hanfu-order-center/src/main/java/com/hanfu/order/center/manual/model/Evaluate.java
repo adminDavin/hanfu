d56f93426b31a7058fc9487f2d1f0933cc71ce10
wangbopeng1 <wangbo_peng@163.com>
2020-01-11 22:16:07 +0800

    修改

--- hanfu-order-center/src/main/java/com/hanfu/order/center/manual/model/Evaluate.java
+++ hanfu-order-center/src/main/java/com/hanfu/order/center/manual/model/Evaluate.java
@@ -4,22 +4,26 @@ import java.io.Serializable;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import io.swagger.annotations.ApiModelProperty;
+
 @SuppressWarnings("serial")
 public class Evaluate implements Serializable{
-private String evaluate;
-private MultipartFile multipartFile;
-public String getEvaluate() {
-	return evaluate;
-}
-public void setEvaluate(String evaluate) {
-	this.evaluate = evaluate;
-}
-public MultipartFile getMultipartFile() {
-	return multipartFile;
-}
-public void setMultipartFile(MultipartFile multipartFile) {
-	this.multipartFile = multipartFile;
-}
+	@ApiModelProperty(required = false, value = "id")
+	private String evaluate;
+	@ApiModelProperty(required = false, value = "id")
+	private MultipartFile multipartFile;
+	public String getEvaluate() {
+		return evaluate;
+	}
+	public void setEvaluate(String evaluate) {
+		this.evaluate = evaluate;
+	}
+	public MultipartFile getMultipartFile() {
+		return multipartFile;
+	}
+	public void setMultipartFile(MultipartFile multipartFile) {
+		this.multipartFile = multipartFile;
+	}
 
 
 }
