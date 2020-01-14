f2b57db1c23c0e0c3b16878402f9fb643eb770b5
davin <davinzhang@outlook.com>
2020-01-15 02:17:06 +0800

    fixed maxed

--- hanfu-group-center/src/main/java/com/hanfu/group/center/controller/GroupController.java
+++ hanfu-group-center/src/main/java/com/hanfu/group/center/controller/GroupController.java
@@ -9,7 +9,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-;
+
 import javax.management.ValueExp;
 import java.sql.SQLOutput;
 import java.text.ParseException;
