945f82ee525e63854f8cf6ce7ca1ec6158a941a0
davin <davinzhang@outlook.com>
2020-02-02 19:52:05 +0800

    fixed

--- hanfu-payment-center/src/main/java/com/hanfu/payment/center/controller/PaymentOrderController.java
+++ hanfu-payment-center/src/main/java/com/hanfu/payment/center/controller/PaymentOrderController.java
@@ -1,11 +1,16 @@
 package com.hanfu.payment.center.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,6 +32,7 @@ import com.hanfu.payment.center.manual.dao.HfOrderDao;
 import com.hanfu.payment.center.manual.model.HfOrderDisplay;
 import com.hanfu.payment.center.manual.model.HfUser;
 import com.hanfu.payment.center.manual.model.OrderStatus;
+import com.hanfu.payment.center.util.PayUtil;
 import com.hanfu.payment.center.util.WXConfigUtil;
 import com.hanfu.utils.response.handler.ResponseEntity;
 import com.hanfu.utils.response.handler.ResponseUtils;
@@ -109,14 +115,53 @@ public class PaymentOrderController {
     
     @ApiOperation(value = "訂單支付后處理", notes = "訂單支付后處理")
     @RequestMapping(value = "/handleWxpay", method = RequestMethod.GET)
-    @ApiImplicitParams({                                                   //商户单号和微信单号  选择其一
-            @ApiImplicitParam(paramType = "query", name = "out_trade_no", value = "商户订单号", required = false),
-            @ApiImplicitParam(paramType = "query", name = "transaction_id", value = "微信订单号", required = false),
-            @ApiImplicitParam(paramType = "query", name = "total_fee", value = "总额", required = true)
-    })
-    public ResponseEntity<JSONObject> refund(String out_trade_no, String transaction_id, int total_fee) throws Exception {
-        ResponseEntity.BodyBuilder builder = ResponseUtils.getBodyBuilder();
-        hfOrderDao.updateHfOrderStatus(out_trade_no, OrderStatus.PROCESS.getOrderStatus(), LocalDateTime.now());        
-        return builder.body(ResponseUtils.getResponseBody(total_fee));
+    public void refund(HttpServletRequest request,HttpServletResponse response) throws Exception {
+        MiniProgramConfig config = new MiniProgramConfig();
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader((ServletInputStream)request.getInputStream()));
+        String line = null;
+        StringBuilder sb = new StringBuilder();
+        while((line = br.readLine()) != null){
+            sb.append(line);
+        }
+        br.close();
+        //sb为微信返回的xml
+        String notityXml = sb.toString();
+        String resXml = "";
+        logger.info("接收到的报文：" + notityXml);
+ 
+        Map map = PayUtil.doXMLParse(notityXml);
+ 
+        String returnCode = (String) map.get("return_code");
+        if("SUCCESS".equals(returnCode)){
+//        hfOrderDao.updateHfOrderStatus(out_trade_no, OrderStatus.PROCESS.getOrderStatus(), LocalDateTime.now());        
+        Map<String, String> validParams = PayUtil.paraFilter(map);  //回调验签时需要去除sign和空值参数
+        String validStr = PayUtil.createLinkString(validParams);//把数组所有元素，按照“参数=参数值”的模式用“&”字符拼接成字符串
+        String sign = PayUtil.sign(validStr, config.getKey(), "utf-8").toUpperCase();//拼装生成服务器端验证的签名
+        // 因为微信回调会有八次之多,所以当第一次回调成功了,那么我们就不再执行逻辑了
+       
+        //根据微信官网的介绍，此处不仅对回调的参数进行验签，还需要对返回的金额与系统订单的金额进行比对等
+        if(sign.equals(map.get("sign"))){
+            /**此处添加自己的业务逻辑代码start**/
+              // bla bla bla....
+            /**此处添加自己的业务逻辑代码end**/
+            //通知微信服务器已经支付成功
+            resXml = "<xml>" + "<return_code><![CDATA[SUCCESS]]></return_code>"
+                    + "<return_msg><![CDATA[OK]]></return_msg>" + "</xml> ";
+        } else {
+            System.out.println("微信支付回调失败!签名不一致");
+        }
+    }else{
+        resXml = "<xml>" + "<return_code><![CDATA[FAIL]]></return_code>"
+                + "<return_msg><![CDATA[报文为空]]></return_msg>" + "</xml> ";
+    }
+    System.out.println(resXml);
+    System.out.println("微信支付回调数据结束");
+
+    BufferedOutputStream out = new BufferedOutputStream(
+            response.getOutputStream());
+    out.write(resXml.getBytes());
+    out.flush();
+    out.close();
     }
 }
--- hanfu-payment-center/src/main/java/com/hanfu/payment/center/util/PayUtil.java
+++ hanfu-payment-center/src/main/java/com/hanfu/payment/center/util/PayUtil.java
@@ -1,270 +1,103 @@
 package com.hanfu.payment.center.util;
 
-import org.slf4j.LoggerFactory;
-import org.springframework.util.StringUtils;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
-import com.alibaba.fastjson.JSON;
-
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringWriter;
-import java.math.BigDecimal;
-import java.net.InetAddress;
-import java.text.SimpleDateFormat;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.SignatureException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.SortedMap;
-import java.util.TreeMap;
-import java.util.UUID;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
-import org.slf4j.Logger;
-
+ 
+import org.apache.commons.codec.digest.DigestUtils;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+ 
 public class PayUtil {
-    static Logger logger = LoggerFactory.getLogger(PayUtil.class.getClass());
-    
     /**
-     * 获取当前机器的ip
-     *
-     * @return String
+     * 签名字符串
+     * @param text 需要签名的字符串
+     * @param key 密钥
+     * @param input_charset 编码格式
+     * @return 签名结果
      */
-    public static String getLocalIp(){
-        InetAddress ia=null;
-        String localip = null;
+    public static String sign(String text, String key, String input_charset) {
+        text = text + "&key=" + key;
+        return DigestUtils.md5Hex(getContentBytes(text, input_charset));
+    }
+    /**
+     * 签名字符串
+     *  @param text 需要签名的字符串
+     * @param sign 签名结果
+     * @param key 密钥
+     * @param input_charset 编码格式
+     * @return 签名结果
+     */
+    public static boolean verify(String text, String sign, String key, String input_charset) {
+        text = text + key;
+        String mysign = DigestUtils.md5Hex(getContentBytes(text, input_charset));
+        if (mysign.equals(sign)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /**
+     * @param content
+     * @param charset
+     * @return
+     * @throws SignatureException
+     * @throws UnsupportedEncodingException
+     */
+    public static byte[] getContentBytes(String content, String charset) {
+        if (charset == null || "".equals(charset)) {
+            return content.getBytes();
+        }
         try {
-            ia=ia.getLocalHost();
-            localip=ia.getHostAddress();
-        } catch (Exception e) {
-            e.printStackTrace();
+            return content.getBytes(charset);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("MD5签名过程中出现错误,指定的编码集不对,您目前指定的编码集是:" + charset);
         }
-        return localip;
-        
-    }
-    
-    /**
-     * Map转换为 Xml
-     * 
-     * @param data
-     * @return Xml
-     * @throws Exception
-     */
-    public static String mapToXml(SortedMap<String, String> map) throws Exception {
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        //防止XXE攻击
-        documentBuilderFactory.setXIncludeAware(false);
-        documentBuilderFactory.setExpandEntityReferences(false);
-        DocumentBuilder documentBuilder= documentBuilderFactory.newDocumentBuilder();
-        org.w3c.dom.Document document = documentBuilder.newDocument();
-        org.w3c.dom.Element root = document.createElement("xml");
-        document.appendChild(root);
-        for (String key: map.keySet()) {
-            String value = map.get(key);
-            if (value == null) {
-                value = "";
-            }
-            value = value.trim();
-            org.w3c.dom.Element filed = document.createElement(key);
-            filed.appendChild(document.createTextNode(value));
-            root.appendChild(filed);
-        }
-        TransformerFactory tf = TransformerFactory.newInstance();
-        Transformer transformer = tf.newTransformer();
-        DOMSource source = new DOMSource(document);
-        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        StringWriter writer = new StringWriter();
-        StreamResult result = new StreamResult(writer);
-        transformer.transform(source, result);
-        String output = writer.getBuffer().toString();
-        try {
-            writer.close();
-        }
-        catch (Exception ex) {
-        }
-        return output;
     }
  
-    
+    private static boolean isValidChar(char ch) {
+        if ((ch >= '0' && ch <= '9') || (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+            return true;
+        if ((ch >= 0x4e00 && ch <= 0x7fff) || (ch >= 0x8000 && ch <= 0x952f))
+            return true;// 简体中文汉字编码
+        return false;
+    }
     /**
-     * 创建签名Sign
-     * 
-     * @param key
-     * @param parameters
-     * @return
-     */ 
-    public static String createSign(SortedMap<String,String> parameters,String key){  
-        StringBuffer sb = new StringBuffer();  
-        Set es = parameters.entrySet();
-        Iterator<?> it = es.iterator();  
-        while(it.hasNext()) {  
-            Map.Entry entry = (Map.Entry)it.next();  
-            String k = (String)entry.getKey();  
-            if(entry.getValue() != null || !"".equals(entry.getValue())) {
-                String v = String.valueOf(entry.getValue());
-                if (null != v && !"".equals(v) && !"sign".equals(k) && !"key".equals(k)) {
-                    sb.append(k + "=" + v + "&");
-                }
-            }  
-        }  
-        sb.append("key=" + key);  
-        String sign = MD5.MD5Encode(sb.toString()).toUpperCase();  
-        return sign;  
-    }
-    
-    
-    /**
-     * XML转换为Map
-     * 
-     * @param strXML
-     * @return Map
-     * @throws Exception
-     */
-    public static Map<String, Object> getMapFromXML(String strXML) throws Exception {
-        try {
-            Map<String, Object> data = new HashMap<String, Object>();
-            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-            //防止XXE攻击
-            documentBuilderFactory.setXIncludeAware(false);
-            documentBuilderFactory.setExpandEntityReferences(false);
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            InputStream stream = new ByteArrayInputStream(strXML.getBytes("UTF-8"));
-            org.w3c.dom.Document doc = documentBuilder.parse(stream);
-            doc.getDocumentElement().normalize();
-            NodeList nodeList = doc.getDocumentElement().getChildNodes();
-            for (int idx = 0; idx < nodeList.getLength(); ++idx) {
-                Node node = nodeList.item(idx);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    org.w3c.dom.Element element = (org.w3c.dom.Element) node;
-                    data.put(element.getNodeName(), element.getTextContent());
-                }
-            }
-            try {
-                stream.close();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-            return data;
-        } catch (Exception ex) {
-            throw ex;
-        }
-    }
-    
-    /**
-     * 生成随机数
-     * 
-     * @return
-     */
-    public static String makeUUID(int len) {
-        return UUID.randomUUID().toString().replaceAll("-", "").substring(0, len);
-    }
-    
-    /**
-     * 获取当前的Timestamp
-     * 
-     * @return
-     */
-    public static String getCurrentTimeStamp() {
-        return Long.toString(System.currentTimeMillis()/1000);
-    }
- 
-    /**
-     * 获取当前的时间
-     * @return
-     */
-    public static long getCurrentTimestampMs() {
-        return System.currentTimeMillis();
-    }
-    
-    /**
-     * 生成订单号
-     * 
-     * @return
-     */
-    public static String generateOrderNo() {    
-        SimpleDateFormat sdf  = new SimpleDateFormat("yyMMdd");
-        return sdf.format(new Date())+makeUUID(16);
-    }
-    
-    /**
-     * 获取当前工程url
-     * 
-     * @param request
-     * @return
-     */
-    public static String getCurrentUrl(HttpServletRequest request){
-        return request.getScheme() +"://" + request.getServerName()  + ":" +request.getServerPort() +request.getContextPath();
-    }
- 
-    public static String getSceneInfo(String wapUrl,String name){
-        Map<String,Map<String,String>> map = new HashMap<String, Map<String,String>>();
-        if(!StringUtils.isEmpty(wapUrl) && !StringUtils.isEmpty(name)){
-            /*{"h5_info": {"type":"Wap","wap_url": "https://pay.qq.com","wap_name": "腾讯充值"}}*/
-            Map<String,String> childmap = new TreeMap<String, String>();
-            childmap.put("type", "Wap");
-            childmap.put("wap_url",wapUrl);
-            childmap.put("wap_name", name);
-            map.put("h5_info", childmap);
-            return JSON.toJSONString(map);
-        }
-        return null;
-    }
-    
- 
-    /**
-     * 转换金额型到整型
-     * @param money
-     * @return
-     */
-    public static String moneyToIntegerStr(Double money){
-        BigDecimal decimal = new BigDecimal(money);
-        int amount = decimal.multiply(new BigDecimal(100))
-            .setScale(0, BigDecimal.ROUND_HALF_UP).intValue();
-        return String.valueOf(amount);
-    }
- 
-    /** 
      * 除去数组中的空值和签名参数
      * @param sArray 签名参数组
      * @return 去掉空值与签名参数后的新签名参数组
      */
     public static Map<String, String> paraFilter(Map<String, String> sArray) {
- 
         Map<String, String> result = new HashMap<String, String>();
- 
         if (sArray == null || sArray.size() <= 0) {
             return result;
         }
- 
         for (String key : sArray.keySet()) {
             String value = sArray.get(key);
             if (value == null || value.equals("") || key.equalsIgnoreCase("sign")
-                || key.equalsIgnoreCase("sign_type")) {
+                    || key.equalsIgnoreCase("sign_type")) {
                 continue;
             }
             result.put(key, value);
         }
- 
         return result;
     }
-    
-    /** 
+    /**
      * 把数组所有元素排序，并按照“参数=参数值”的模式用“&”字符拼接成字符串
      * @param params 需要排序并参与字符拼接的参数组
      * @return 拼接后字符串
@@ -276,7 +109,7 @@ public class PayUtil {
         for (int i = 0; i < keys.size(); i++) {
             String key = keys.get(i);
             String value = params.get(key);
-            if (i == keys.size() - 1) {//拼接时，不包括最后一个&字符
+            if (i == keys.size() - 1) {// 拼接时，不包括最后一个&字符
                 prestr = prestr + key + "=" + value;
             } else {
                 prestr = prestr + key + "=" + value + "&";
@@ -284,5 +117,117 @@ public class PayUtil {
         }
         return prestr;
     }
-     
+    /**
+     *
+     * @param requestUrl 请求地址
+     * @param requestMethod 请求方法
+     * @param outputStr 参数
+     */
+    public static String httpRequest(String requestUrl,String requestMethod,String outputStr) {
+        // 创建SSLContext
+        StringBuffer buffer = null;
+        try {
+            URL url = new URL(requestUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod(requestMethod);
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.connect();
+            //往服务器端写内容
+            if (null != outputStr) {
+                OutputStream os = conn.getOutputStream();
+                os.write(outputStr.getBytes("utf-8"));
+                os.close();
+            }
+            // 读取服务器端返回的内容
+            InputStream is = conn.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is, "utf-8");
+            BufferedReader br = new BufferedReader(isr);
+            buffer = new StringBuffer();
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                buffer.append(line);
+            }
+            br.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return buffer.toString();
+    }
+    public static String urlEncodeUTF8(String source){
+        String result=source;
+        try {
+            result=java.net.URLEncoder.encode(source, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return result;
+    }
+    /**
+     * 解析xml,返回第一级元素键值对。如果第一级元素有子节点，则此节点的值是子节点的xml数据。
+     * @param strxml
+     * @return
+     * @throws JDOMException
+     * @throws IOException
+     */
+    public static Map doXMLParse(String strxml) throws Exception {
+        if(null == strxml || "".equals(strxml)) {
+            return null;
+        }
+ 
+        Map m = new HashMap();
+        InputStream in = String2Inputstream(strxml);
+        SAXBuilder builder = new SAXBuilder();
+        Document doc = builder.build(in);
+        Element root = doc.getRootElement();
+        List list = root.getChildren();
+        Iterator it = list.iterator();
+        while(it.hasNext()) {
+            Element e = (Element) it.next();
+            String k = e.getName();
+            String v = "";
+            List children = e.getChildren();
+            if(children.isEmpty()) {
+                v = e.getTextNormalize();
+            } else {
+                v = getChildrenText(children);
+            }
+ 
+            m.put(k, v);
+        }
+ 
+        //关闭流
+        in.close();
+ 
+        return m;
+    }
+    /**
+     * 获取子结点的xml
+     * @param children
+     * @return String
+     */
+    public static String getChildrenText(List children) {
+        StringBuffer sb = new StringBuffer();
+        if(!children.isEmpty()) {
+            Iterator it = children.iterator();
+            while(it.hasNext()) {
+                Element e = (Element) it.next();
+                String name = e.getName();
+                String value = e.getTextNormalize();
+                List list = e.getChildren();
+                sb.append("<" + name + ">");
+                if(!list.isEmpty()) {
+                    sb.append(getChildrenText(list));
+                }
+                sb.append(value);
+                sb.append("</" + name + ">");
+            }
+        }
+ 
+        return sb.toString();
+    }
+    public static InputStream String2Inputstream(String str) {
+        return new ByteArrayInputStream(str.getBytes());
+    }
 }
