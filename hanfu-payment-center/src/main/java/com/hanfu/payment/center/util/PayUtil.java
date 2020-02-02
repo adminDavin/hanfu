3475d8b5c4e51d5a51cde496280984148fe700f9
davin <davinzhang@outlook.com>
2020-02-02 20:05:57 +0800

    fixed

--- hanfu-payment-center/src/main/java/com/hanfu/payment/center/util/PayUtil.java
+++ hanfu-payment-center/src/main/java/com/hanfu/payment/center/util/PayUtil.java
@@ -18,10 +18,10 @@ import java.util.List;
 import java.util.Map;
  
 import org.apache.commons.codec.digest.DigestUtils;
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.JDOMException;
-import org.jdom.input.SAXBuilder;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
  
 public class PayUtil {
     /**
