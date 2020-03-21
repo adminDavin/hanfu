6d7493f924dda2940193d16848a29f10f3befaee
sunwangda <2451203734@qq.com>
2020-03-22 01:18:15 +0800

    huodongxiangguan

--- hanfu-product-center/src/main/java/com/hanfu/product/center/manual/model/ProductActivityInfo.java
+++ hanfu-product-center/src/main/java/com/hanfu/product/center/manual/model/ProductActivityInfo.java
@@ -13,7 +13,7 @@ public class ProductActivityInfo {
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
 	private Date startTime;
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-	private Date endtime;
+	private Date endTime;
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
 	private Date createTime;
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
@@ -54,11 +54,11 @@ public class ProductActivityInfo {
 	public void setStartTime(Date startTime) {
 		this.startTime = startTime;
 	}
-	public Date getEndtime() {
-		return endtime;
+	public Date getEndTime() {
+		return endTime;
 	}
-	public void setEndtime(Date endtime) {
-		this.endtime = endtime;
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
 	}
 	public Date getCreateTime() {
 		return createTime;
