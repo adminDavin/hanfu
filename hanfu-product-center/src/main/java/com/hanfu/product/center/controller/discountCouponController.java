d00ab8a70790e794fb53a2a50866e803947112b7
shihaoluoling <441168597@qq.com>
2020-03-24 16:29:52 +0800

    3.24

--- hanfu-product-center/src/main/java/com/hanfu/product/center/controller/discountCouponController.java
+++ hanfu-product-center/src/main/java/com/hanfu/product/center/controller/discountCouponController.java
@@ -21,6 +21,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.*;
@@ -44,16 +45,17 @@ private FileDescMapper fileDescMapper;
         System.out.println(scopeTypeEnum);
         ResponseEntity.BodyBuilder builder = ResponseUtils.getBodyBuilder(HttpStatus.OK);
         String arr[];
-        arr = FileMangeService.uploadFile(fileInfo.getBytes(), String.valueOf(discountCoupon.getCreator()));
         FileDesc fileDesc = new FileDesc();
-        fileDesc.setFileName(fileInfo.getName());
-        fileDesc.setGroupName(arr[0]);
-        fileDesc.setRemoteFilename(arr[1]);
-        fileDesc.setUserId(discountCoupon.getCreator());
-        fileDesc.setCreateTime(LocalDateTime.now());
-        fileDesc.setModifyTime(LocalDateTime.now());
-        fileDescMapper.insertSelective(fileDesc);
-
+        if (fileInfo!=null){
+            arr = FileMangeService.uploadFile(fileInfo.getBytes(), String.valueOf(discountCoupon.getCreator()));
+            fileDesc.setFileName(fileInfo.getName());
+            fileDesc.setGroupName(arr[0]);
+            fileDesc.setRemoteFilename(arr[1]);
+            fileDesc.setUserId(discountCoupon.getCreator());
+            fileDesc.setCreateTime(LocalDateTime.now());
+            fileDesc.setModifyTime(LocalDateTime.now());
+            fileDescMapper.insertSelective(fileDesc);
+        }
 
         discountCoupon.setCreateDate(LocalDateTime.now());
         discountCoupon.setModifyDate(LocalDateTime.now());
@@ -87,7 +89,28 @@ private FileDescMapper fileDescMapper;
         ResponseEntity.BodyBuilder builder = ResponseUtils.getBodyBuilder(HttpStatus.OK);
         DiscountCouponExample discountCouponExample = new DiscountCouponExample();
         discountCouponExample.createCriteria().andIdDeletedEqualTo((byte) 0).andBossIdEqualTo(bossId);
-        return builder.body(ResponseUtils.getResponseBody(discountCouponMapper.selectByExample(discountCouponExample)));
+        List<DiscountCoupon> discountCoupons= discountCouponMapper.selectByExample(discountCouponExample);
+        discountCoupons.forEach(discountCoupon -> {
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            try {
+                Date date1 = df.parse(df.format(discountCoupon.getStartTime()));
+                Date date2 = df.parse(df.format(discountCoupon.getStopTime()));
+                Date date3 = df.parse(df.format(new Date()));
+                System.out.println("---1---"+date1+"---2---"+date2+"----3---"+date3);
+                // 得到微秒级别的差值
+                long diff = date3.getTime() - date1.getTime();
+                long diff1= date2.getTime() - date3.getTime();
+                // 将级别提升到天
+                long days = diff /(1000 * 60); //当前时间减去开始时间
+                long days2 = diff1 /(1000 * 60);//结束时间减去当前时间
+                System.out.println(days+"相减时间");
+                System.out.println(days2+"相减时间2");
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        });
+
+        return builder.body(ResponseUtils.getResponseBody(discountCoupons));
     }
 
 
