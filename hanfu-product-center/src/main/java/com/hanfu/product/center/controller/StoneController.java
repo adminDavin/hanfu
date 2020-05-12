3b78abef7e16eecabf6be0501a92612411b58e83
shihaoluoling <441168597@qq.com>
2020-05-12 17:38:22 +0800

    5.12

--- hanfu-product-center/src/main/java/com/hanfu/product/center/controller/StoneController.java
+++ hanfu-product-center/src/main/java/com/hanfu/product/center/controller/StoneController.java
@@ -206,13 +206,13 @@ public class StoneController {
 			fileDesc.setIsDeleted((short) 0);
 			fileDescMapper.insert(fileDesc);
 			HfStonePictureExample hfStonePictureExample = new HfStonePictureExample();
-			hfStonePictureExample.createCriteria().andStoneIdEqualTo(stoneId).andIsDeletedEqualTo((byte) 0);
+			hfStonePictureExample.createCriteria().andStoneIdEqualTo(stoneId).andIsDeletedEqualTo((byte) 0).andTypeEqualTo("avatar");
             List<HfStonePicture> hfStonePictures= hfStonePictureMapper.selectByExample(hfStonePictureExample);
-			if (hfStonePictures.size()!=0){
+			if (hfStonePictures.size()!=0&&type==null){
 			    HfStonePicture hfStonePicture = new HfStonePicture();
 			    hfStonePicture.setIsDeleted((byte) 1);
                 HfStonePictureExample hfStonePictureExample1 = new HfStonePictureExample();
-                hfStonePictureExample1.createCriteria().andStoneIdEqualTo(stoneId).andIsDeletedEqualTo((byte) 0);
+                hfStonePictureExample1.createCriteria().andStoneIdEqualTo(stoneId).andIsDeletedEqualTo((byte) 0).andTypeEqualTo("avatar");
                 hfStonePictureMapper.updateByExampleSelective(hfStonePicture,hfStonePictureExample1);
             }
             HfStonePicture picture = new HfStonePicture();
