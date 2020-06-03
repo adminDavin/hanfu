5f43c816abb7513ec34a6f0eadf3bdda0453f0b5
shihaoluoling <441168597@qq.com>
2020-06-03 15:21:26 +0800

    6.1

--- hanfu-user-center/src/main/java/com/hanfu/user/center/config/WxLoginConfig.java
+++ hanfu-user-center/src/main/java/com/hanfu/user/center/config/WxLoginConfig.java
@@ -42,9 +42,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class WxLoginConfig {
-    @Autowired
     public static HfUserMapper hfUserMapper;
-	public static String APPID ="wx2641aaa105c07dd4";
+
+    @Autowired
+    public void setUserMapper(HfUserMapper hfUserMapper) {
+        WxLoginConfig.hfUserMapper = hfUserMapper;
+    }
+
+    public static String APPID ="wx2641aaa105c07dd4";
 	public static String SECRET ="fb26dde971b62de61c4573b12bd5f5da";
 	public static String GRANTTYPE = "authorization_code";
     public static CloseableHttpClient client = HttpClients.createDefault();
