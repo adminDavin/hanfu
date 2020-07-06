c4cb8e5e5b2951cdb732f5a8d6404f56fbb05df9
shihaoluoling <441168597@qq.com>
2020-07-06 14:41:25 +0800

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/dao/DcCompanyMapper.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/dao/DcUserMapper.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/model/DcCompany.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/model/DcCompanyExample.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/model/DcUser.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/model/DcUserExample.java
    #	hanfu-dichan-center/src/main/resources/mappers/DcCompanyMapper.xml
    #	hanfu-dichan-center/src/main/resources/mappers/DcUserMapper.xml

--- hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/config/MyInterceptor.java
+++ hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/config/MyInterceptor.java
@@ -25,8 +25,6 @@ public class MyInterceptor implements HandlerInterceptor {
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        request.getServletContext().removeAttribute("getServletContext");
-        request.getServletContext().removeAttribute("getServletContextType");
         logger.info("整个请求都处理完咯，DispatcherServlet也渲染了对应的视图咯，此时我可以做一些清理的工作了");
     }
 }
