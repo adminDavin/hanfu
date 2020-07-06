ac2557875b60707bf21d470e6b66502de68a531b
sunwangda <2451203734@qq.com>
2020-07-06 11:49:09 +0800

    1

--- hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/controller/DcCategoryController.java
+++ hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/controller/DcCategoryController.java
@@ -2,9 +2,9 @@ package com.hanfu.dichan.center.controller;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 import java.util.Date;
-import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -20,11 +20,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.alibaba.fastjson.JSONObject;
+import com.hanfu.dichan.center.request.CategoryRequest;
 import com.hanfu.utils.response.handler.ResponseEntity;
-import com.hanfu.utils.response.handler.ResponseUtils;
 import com.hanfu.utils.response.handler.ResponseEntity.BodyBuilder;
+import com.hanfu.utils.response.handler.ResponseUtils;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -32,13 +34,39 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @RequestMapping("/wareHouse")
 @Api
-public class WareHouseController {
+public class DcCategoryController {
 	protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	private static final String REST_URL_PREFIX = "https://www.tjsichuang.cn:1443/api/user/";
 
 	@Autowired
 	private RestTemplate restTemplate;
+	
+	@ApiOperation(value = "添加类目", notes = "添加系统支持的商品类目")
+	@RequestMapping(value = "/addCategory", method = RequestMethod.POST)
+	public ResponseEntity<JSONObject> AddCategory(CategoryRequest request,MultipartFile fileInfo,HttpServletRequest requests) throws Exception {
+		BodyBuilder builder = ResponseUtils.getBodyBuilder(HttpStatus.OK);
+		HfCategory category = new HfCategory();
+		String uuid = UUID.randomUUID().toString();
+//		uuid = uuid.replace("-", "");
+//		if(fileInfo != null) {
+//			category.setFileId(updateCategoryPicture(fileInfo,uuid,"无"));
+//		}
+		Integer bossId=null;
+		if (requests.getServletContext().getAttribute("getServletContext")!=null){
+			if (requests.getServletContext().getAttribute("getServletContextType").equals("boss")){
+				bossId = (Integer) requests.getServletContext().getAttribute("getServletContext");
+			}
+		}
+		category.setBossId(bossId);
+		category.setLevelId(request.getLevelId());
+		category.setHfName(request.getCategory());
+		category.setParentCategoryId(request.getParentCategoryId());
+		category.setCreateTime(LocalDateTime.now());
+		category.setModifyTime(LocalDateTime.now());
+		category.setIsDeleted((short) 0);
+		return builder.body(ResponseUtils.getResponseBody(hfCategoryMapper.insert(category)));
+	}
 
 	@ApiOperation(value = "获取类目根据条件", notes = "获取类目根据条件")
 	@RequestMapping(value = "/getCategoryByInfo", method = RequestMethod.GET)
