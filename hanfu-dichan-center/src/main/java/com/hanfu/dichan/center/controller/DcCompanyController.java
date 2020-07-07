8f2432f0cb783cef2d11e458a3cbdb781cc73359
shihaoluoling <441168597@qq.com>
2020-07-07 11:49:11 +0800

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/dao/DcCompanyMapper.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/dao/DcUserMapper.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/model/DcCompany.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/model/DcCompanyExample.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/model/DcUser.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/model/DcUserExample.java
    #	hanfu-dichan-center/src/main/resources/mappers/DcCompanyMapper.xml
    #	hanfu-dichan-center/src/main/resources/mappers/DcUserMapper.xml

--- hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/controller/DcCompanyController.java
+++ hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/controller/DcCompanyController.java
@@ -11,6 +11,7 @@ import com.hanfu.utils.response.handler.ResponseEntity;
 import com.hanfu.utils.response.handler.ResponseUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang.StringEscapeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -54,15 +55,27 @@ private DcRichTextMapper dcRichTextMapper;
     @RequestMapping(value = "/addText", method = RequestMethod.POST)
     public ResponseEntity<JSONObject> addText(DcRichText dcRichText) throws Exception {
         ResponseEntity.BodyBuilder builder = ResponseUtils.getBodyBuilder(HttpStatus.OK);
+        System.out.println(dcRichText.getRichText().replace('\"','\''));
+        dcRichText.setRichText(dcRichText.getRichText().replace('\"','\''));
+
+//        dcRichText.setRichText(StringEscapeUtils.escapeHtml(dcRichText.getRichText()));
+//        System.out.println(dcRichText.getRichText());
         dcRichTextMapper.insertSelective(dcRichText);
         return builder.body(ResponseUtils.getResponseBody(0));
     }
     @ApiOperation(value = "查询富文本", notes = "查询富文本")
-    @RequestMapping(value = "/selectText", method = RequestMethod.POST)
-    public List<DcRichText> selectText(Integer projectId, String type) throws Exception {
+    @RequestMapping(value = "/selectText", method = RequestMethod.GET)
+    public ResponseEntity<JSONObject> selectText(Integer projectId, String type) throws Exception {
         ResponseEntity.BodyBuilder builder = ResponseUtils.getBodyBuilder(HttpStatus.OK);
         DcRichTextExample dcRichTextExample = new DcRichTextExample();
         dcRichTextExample.createCriteria().andProjectIdEqualTo(projectId).andTextTypeEqualTo(type).andIsDeletedEqualTo((byte) 0);
-        return dcRichTextMapper.selectByExample(dcRichTextExample);
+        String a = dcRichTextMapper.selectByExample(dcRichTextExample).get(0).getRichText();
+        System.out.println(a);
+//        String jieguo = a.substring(a.indexOf("")+1,a.indexOf("\"));
+//        JSONObject jsonObject1 =JSONObject.parseObject(dcRichTextMapper.selectByExample(dcRichTextExample).toString());
+//        Object myclass = JSONObject.parseObject(a , Object.class);// jsonStr 是String类型。
+//        String newJson = StringEscapeUtils.unescapeHtml(a);
+//        System.out.println(newJson);
+        return builder.body(ResponseUtils.getResponseBody(a));
     }
 }
