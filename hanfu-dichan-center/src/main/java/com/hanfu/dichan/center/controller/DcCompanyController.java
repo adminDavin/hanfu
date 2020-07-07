5f77e881d2bebdb24f02ac9be90dcec69120d132
shihaoluoling <441168597@qq.com>
2020-07-07 09:44:52 +0800

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/dao/DcCompanyMapper.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/dao/DcUserMapper.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/model/DcCompany.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/model/DcCompanyExample.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/model/DcUser.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/model/DcUserExample.java
    #	hanfu-dichan-center/src/main/resources/mappers/DcCompanyMapper.xml
    #	hanfu-dichan-center/src/main/resources/mappers/DcUserMapper.xml

---
+++ hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/controller/DcCompanyController.java
@@ -0,0 +1,13 @@
+package com.hanfu.dichan.center.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api
+@RequestMapping("/company")
+@CrossOrigin
+public class DcCompanyController {
+}
