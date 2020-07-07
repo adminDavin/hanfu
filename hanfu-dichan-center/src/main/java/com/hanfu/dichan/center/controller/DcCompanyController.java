0287c203aea52e7ae753468a3379bbe54b662d4e
shihaoluoling <441168597@qq.com>
2020-07-07 10:16:33 +0800

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/dao/DcCompanyMapper.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/dao/DcUserMapper.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/model/DcCompany.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/model/DcCompanyExample.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/model/DcUser.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/model/DcUserExample.java
    #	hanfu-dichan-center/src/main/resources/mappers/DcCompanyMapper.xml
    #	hanfu-dichan-center/src/main/resources/mappers/DcUserMapper.xml

--- hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/controller/DcCompanyController.java
+++ hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/controller/DcCompanyController.java
@@ -3,7 +3,10 @@ package com.hanfu.dichan.center.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.hanfu.common.service.FileMangeService;
 import com.hanfu.dichan.center.dao.DcFileDescMapper;
+import com.hanfu.dichan.center.dao.DcRichTextMapper;
 import com.hanfu.dichan.center.model.DcFileDesc;
+import com.hanfu.dichan.center.model.DcRichText;
+import com.hanfu.dichan.center.model.DcRichTextExample;
 import com.hanfu.utils.response.handler.ResponseEntity;
 import com.hanfu.utils.response.handler.ResponseUtils;
 import io.swagger.annotations.Api;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @Api
@@ -25,7 +29,8 @@ import java.time.LocalDateTime;
 public class DcCompanyController {
     @Autowired
     private DcFileDescMapper dcFileDescMapper;
-
+@Autowired
+private DcRichTextMapper dcRichTextMapper;
     @ApiOperation(value = "上传图片", notes = "上传图片")
     @RequestMapping(value = "/fileUpLoad", method = RequestMethod.POST)
     public ResponseEntity<JSONObject> fileUpLoad(MultipartFile file) throws Exception {
@@ -44,5 +49,20 @@ public class DcCompanyController {
         dcFileDescMapper.insert(fileDesc);
         String a = "https://www.tjsichuang.cn:1443/api/dichan/category/getPicture?id=";
         return builder.body(ResponseUtils.getResponseBody(a+fileDesc.getId()));
+    }
+    @ApiOperation(value = "添加富文本", notes = "添加富文本")
+    @RequestMapping(value = "/addText", method = RequestMethod.POST)
+    public ResponseEntity<JSONObject> addText(DcRichText dcRichText) throws Exception {
+        ResponseEntity.BodyBuilder builder = ResponseUtils.getBodyBuilder(HttpStatus.OK);
+        dcRichTextMapper.insertSelective(dcRichText);
+        return builder.body(ResponseUtils.getResponseBody(0));
+    }
+    @ApiOperation(value = "查询富文本", notes = "查询富文本")
+    @RequestMapping(value = "/selectText", method = RequestMethod.POST)
+    public List<DcRichText> selectText(Integer projectId, String type) throws Exception {
+        ResponseEntity.BodyBuilder builder = ResponseUtils.getBodyBuilder(HttpStatus.OK);
+        DcRichTextExample dcRichTextExample = new DcRichTextExample();
+        dcRichTextExample.createCriteria().andProjectIdEqualTo(projectId).andTextTypeEqualTo(type).andIsDeletedEqualTo((byte) 0);
+        return dcRichTextMapper.selectByExample(dcRichTextExample);
     }
 }
