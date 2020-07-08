7ef86709a1e1f45ec971f6d387a0bec9fd297a8d
sunwangda <2451203734@qq.com>
2020-07-08 15:56:39 +0800

    1

--- hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/controller/DcCategoryController.java
+++ hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/controller/DcCategoryController.java
@@ -8,7 +8,9 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import javax.imageio.ImageIO;
@@ -20,6 +22,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
@@ -35,10 +38,15 @@ import com.alibaba.fastjson.JSONObject;
 import com.hanfu.common.service.FileMangeService;
 import com.hanfu.dichan.center.dao.DcCategoryMapper;
 import com.hanfu.dichan.center.dao.DcFileDescMapper;
+import com.hanfu.dichan.center.dao.DcGeneralFileMapper;
 import com.hanfu.dichan.center.manual.dao.ManualDao;
+import com.hanfu.dichan.center.manual.model.CatrgoryOrDetail;
 import com.hanfu.dichan.center.model.DcCategory;
 import com.hanfu.dichan.center.model.DcCategoryExample;
 import com.hanfu.dichan.center.model.DcFileDesc;
+import com.hanfu.dichan.center.model.DcFileDescExample;
+import com.hanfu.dichan.center.model.DcGeneralFile;
+import com.hanfu.dichan.center.model.DcGeneralFileExample;
 import com.hanfu.dichan.center.request.CategoryRequest;
 import com.hanfu.utils.response.handler.ResponseEntity;
 import com.hanfu.utils.response.handler.ResponseEntity.BodyBuilder;
@@ -68,6 +76,9 @@ public class DcCategoryController {
 
 	@Autowired
 	private ManualDao manualDao;
+
+	@Autowired
+	private DcGeneralFileMapper dcGeneralFileMapper;
 
 	@ApiOperation(value = "添加类目", notes = "添加类目")
 	@RequestMapping(value = "/addCategory", method = RequestMethod.POST)
@@ -104,6 +115,7 @@ public class DcCategoryController {
 			fileMangeService.deleteFile(fileDesc.getGroupName(), fileDesc.getRemoteFilename());
 			dcFileDescMapper.deleteByPrimaryKey(hfCategory.getFileId());
 		}
+		dcCategoryMapper.deleteByPrimaryKey(categoryId);
 		return builder.body(ResponseUtils.getResponseBody(dcFileDescMapper.deleteByPrimaryKey(categoryId)));
 	}
 
@@ -156,21 +168,33 @@ public class DcCategoryController {
 	public ResponseEntity<JSONObject> getCategoryByInfo(Integer parentCategoryId, Integer projectId) throws Exception {
 		BodyBuilder builder = ResponseUtils.getBodyBuilder(HttpStatus.OK);
 		Integer bossId = 1;
+		Map<String, Object> map = new HashMap<String, Object>();
 		DcCategoryExample categoryExample = new DcCategoryExample();
 		categoryExample.createCriteria().andBossIdEqualTo(bossId).andParentCategoryIdEqualTo(parentCategoryId)
 				.andIsDeletedEqualTo((short) 0);
-		if(parentCategoryId == -1) {
+		if (parentCategoryId == -1) {
 			categoryExample.createCriteria().andProjectIdEqualTo(projectId);
 		}
 		List<DcCategory> list = dcCategoryMapper.selectByExample(categoryExample);
-		return builder.body(ResponseUtils.getResponseBody(list));
+		if (CollectionUtils.isEmpty(list) || list.size() == 0) {
+			DcGeneralFileExample example = new DcGeneralFileExample();
+			example.createCriteria().andGeneralIdEqualTo(parentCategoryId);
+			List<DcGeneralFile> files = dcGeneralFileMapper.selectByExample(example);
+			map.put("type", "detail");
+			map.put("list", files);
+			return builder.body(ResponseUtils.getResponseBody(map));
+		}
+		map.put("type", "category");
+		map.put("list", list);
+		return builder.body(ResponseUtils.getResponseBody(map));
 	}
-	
+
 	@ApiOperation(value = "根据名字等级获取类目", notes = "根据名字等级获取类目")
 	@RequestMapping(value = "/getCategoryByNameOrLevel", method = RequestMethod.GET)
-	public ResponseEntity<JSONObject> getCategoryByNameOrLevel(HttpServletRequest request,Integer level, String name, Integer projectId) throws Exception {
+	public ResponseEntity<JSONObject> getCategoryByNameOrLevel(HttpServletRequest request, Integer level, String name,
+			Integer projectId) throws Exception {
 		BodyBuilder builder = ResponseUtils.getBodyBuilder(HttpStatus.OK);
-		Integer bossId =1;
+		Integer bossId = 1;
 //		if (request.getServletContext().getAttribute("getServletContextType").equals("boss")){
 //			System.out.println("request.getServletContext().getAttribute得到全局数据："+request.getServletContext().getAttribute("getServletContext"));
 //			if (request.getServletContext().getAttribute("getServletContext")!=null){
@@ -183,8 +207,8 @@ public class DcCategoryController {
 		h.setBossId(bossId);
 		h.setProjectId(projectId);
 		List<DcCategory> list = manualDao.findCategoryByInfo(h);
-        return builder.body(ResponseUtils.getResponseBody(list));
-    }
+		return builder.body(ResponseUtils.getResponseBody(list));
+	}
 
 	@ApiOperation(value = "获取图片", notes = "获取图片")
 	@RequestMapping(value = "/getPicture", method = RequestMethod.GET)
@@ -218,6 +242,71 @@ public class DcCategoryController {
 		fileManageService.deleteFile(fileDesc.getGroupName(), fileDesc.getRemoteFilename());
 		//
 		return fileDesc.getId();
+	}
+
+	@ApiOperation(value = "添加类目详情", notes = "添加类目详情")
+	@RequestMapping(value = "/addCategoryDetail", method = RequestMethod.POST)
+	public ResponseEntity<JSONObject> addCategoryDetail(Integer id, MultipartFile file) throws Exception {
+		BodyBuilder builder = ResponseUtils.getBodyBuilder(HttpStatus.OK);
+		FileMangeService fileManageService = new FileMangeService();
+		String arr[];
+		arr = fileManageService.uploadFile(file.getBytes(), "1");
+		DcFileDesc fileDesc = new DcFileDesc();
+		fileDesc.setFileName(file.getName());
+		fileDesc.setGroupName(arr[0]);
+		fileDesc.setRemoteFilename(arr[1]);
+		fileDesc.setCreateTime(LocalDateTime.now());
+		fileDesc.setModifyTime(LocalDateTime.now());
+		fileDesc.setIsDeleted((short) 0);
+		dcFileDescMapper.insert(fileDesc);
+		DcGeneralFile dcGeneralFile = new DcGeneralFile();
+		dcGeneralFile.setFileId(fileDesc.getId());
+		dcGeneralFile.setGeneralId(id);
+		dcGeneralFile.setCreateTime(LocalDateTime.now());
+		dcGeneralFile.setModifyTime(LocalDateTime.now());
+		dcGeneralFile.setIsDeleted((byte) 0);
+		dcGeneralFileMapper.insert(dcGeneralFile);
+		return builder.body(ResponseUtils.getResponseBody(1));
+	}
+	
+	@ApiOperation(value = "删除类目详情", notes = "删除类目详情")
+	@RequestMapping(value = "/deleteCategoryDetail", method = RequestMethod.GET)
+	public ResponseEntity<JSONObject> deleteCategoryDetail(Integer id) throws Exception {
+		BodyBuilder builder = ResponseUtils.getBodyBuilder(HttpStatus.OK);
+		DcGeneralFile dcGeneralFile = dcGeneralFileMapper.selectByPrimaryKey(id);
+		dcGeneralFileMapper.deleteByPrimaryKey(id);
+		DcFileDesc fileDesc = dcFileDescMapper.selectByPrimaryKey(dcGeneralFile.getFileId());
+		FileMangeService fileMangeService = new FileMangeService();
+		fileMangeService.deleteFile(fileDesc.getGroupName(), fileDesc.getFileName());
+		dcGeneralFileMapper.deleteByPrimaryKey(id);
+		return builder.body(ResponseUtils.getResponseBody(fileDesc.getId()));
+	}
+	
+	@ApiOperation(value = "修改类目详情", notes = "修改类目详情")
+	@RequestMapping(value = "/updateCategoryDetail", method = RequestMethod.POST)
+	public ResponseEntity<JSONObject> updateCategoryDetail(Integer id, MultipartFile file) throws Exception {
+		BodyBuilder builder = ResponseUtils.getBodyBuilder(HttpStatus.OK);
+		DcGeneralFile dcGeneralFile = dcGeneralFileMapper.selectByPrimaryKey(id);
+		DcFileDesc fileDesc = dcFileDescMapper.selectByPrimaryKey(dcGeneralFile.getFileId());
+		FileMangeService fileManageService = new FileMangeService();
+		String arr[];
+		arr = fileManageService.uploadFile(file.getBytes(), "1");
+		fileManageService.deleteFile(fileDesc.getGroupName(), fileDesc.getRemoteFilename());
+		fileDesc.setGroupName(arr[0]);
+		fileDesc.setRemoteFilename(arr[1]);
+		fileDesc.setModifyTime(LocalDateTime.now());
+		dcFileDescMapper.updateByPrimaryKey(fileDesc);
+		return builder.body(ResponseUtils.getResponseBody(dcGeneralFile.getId()));
+	}
+	
+	@ApiOperation(value = "查询类目详情", notes = "查询类目详情")
+	@RequestMapping(value = "/selectCategoryDetail", method = RequestMethod.GET)
+	public ResponseEntity<JSONObject> selectCategoryDetail(Integer categoryId) throws Exception {
+		BodyBuilder builder = ResponseUtils.getBodyBuilder(HttpStatus.OK);
+		DcGeneralFileExample example = new DcGeneralFileExample();
+		example.createCriteria().andGeneralIdEqualTo(categoryId);
+		List<DcGeneralFile> files = dcGeneralFileMapper.selectByExample(example);
+		return builder.body(ResponseUtils.getResponseBody(files));
 	}
 
 	@InitBinder
