4c1e4d0f4ae9136f2ea601c26334b90040fdeab9
shihaoluoling <441168597@qq.com>
2020-07-08 17:45:11 +0800

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/dao/DcCompanyMapper.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/dao/DcUserMapper.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/model/DcCompany.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/model/DcCompanyExample.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/model/DcUser.java
    #	hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/model/DcUserExample.java
    #	hanfu-dichan-center/src/main/resources/mappers/DcCompanyMapper.xml
    #	hanfu-dichan-center/src/main/resources/mappers/DcUserMapper.xml

--- hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/controller/DcCompanyController.java
+++ hanfu-dichan-center/src/main/java/com/hanfu/dichan/center/controller/DcCompanyController.java
@@ -3,10 +3,9 @@ package com.hanfu.dichan.center.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.hanfu.common.service.FileMangeService;
 import com.hanfu.dichan.center.dao.DcFileDescMapper;
+import com.hanfu.dichan.center.dao.DcGeneralFileMapper;
 import com.hanfu.dichan.center.dao.DcRichTextMapper;
-import com.hanfu.dichan.center.model.DcFileDesc;
-import com.hanfu.dichan.center.model.DcRichText;
-import com.hanfu.dichan.center.model.DcRichTextExample;
+import com.hanfu.dichan.center.model.*;
 import com.hanfu.utils.response.handler.ResponseEntity;
 import com.hanfu.utils.response.handler.ResponseUtils;
 import io.swagger.annotations.Api;
@@ -21,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -32,6 +32,9 @@ public class DcCompanyController {
     private DcFileDescMapper dcFileDescMapper;
 @Autowired
 private DcRichTextMapper dcRichTextMapper;
+@Autowired
+private DcGeneralFileMapper dcGeneralFileMapper;
+
     @ApiOperation(value = "上传图片", notes = "上传图片")
     @RequestMapping(value = "/fileUpLoad", method = RequestMethod.POST)
     public ResponseEntity<JSONObject> fileUpLoad(MultipartFile file) throws Exception {
@@ -98,5 +101,52 @@ private DcRichTextMapper dcRichTextMapper;
 
         dcRichTextMapper.updateByPrimaryKeySelective(dcRichText);
         return builder.body(ResponseUtils.getResponseBody(0));
+    }
+    @ApiOperation(value = "添加项目概况图", notes = "添加项目概况图")
+    @RequestMapping(value = "/addProject", method = RequestMethod.POST)
+    public ResponseEntity<JSONObject> addProject(MultipartFile file,Integer projectId) throws Exception {
+        ResponseEntity.BodyBuilder builder = ResponseUtils.getBodyBuilder(HttpStatus.OK);
+        String arr[];
+        FileMangeService fileMangeService = new FileMangeService();
+        arr = fileMangeService.uploadFile(file.getBytes(), String.valueOf("-1"));
+        DcFileDesc fileDesc = new DcFileDesc();
+        fileDesc.setFileName(file.getName());
+        fileDesc.setGroupName(arr[0]);
+        fileDesc.setRemoteFilename(arr[1]);
+        fileDesc.setUserId(-1);
+        fileDesc.setCreateTime(LocalDateTime.now());
+        fileDesc.setModifyTime(LocalDateTime.now());
+        fileDesc.setIsDeleted((short) 0);
+        dcFileDescMapper.insert(fileDesc);
+        DcGeneralFile dcGeneralFile = new DcGeneralFile();
+        dcGeneralFile.setFileId(fileDesc.getId());
+        dcGeneralFile.setGeneralId(projectId);
+        dcGeneralFile.setFileType(General.GeneralTypeEnum.GENERAL_TYPE_ENUM.getGeneralType());
+        return builder.body(ResponseUtils.getResponseBody(0));
+    }
+    @ApiOperation(value = "查询项目概况图", notes = "查询项目概况图")
+    @RequestMapping(value = "/selectProject", method = RequestMethod.GET)
+    public ResponseEntity<JSONObject> selectProject(Integer projectId) throws Exception {
+        ResponseEntity.BodyBuilder builder = ResponseUtils.getBodyBuilder(HttpStatus.OK);
+        DcGeneralFileExample dcGeneralFileExample = new DcGeneralFileExample();
+        dcGeneralFileExample.createCriteria().andFileTypeEqualTo(General.GeneralTypeEnum.GENERAL_TYPE_ENUM.getGeneralType()).andIsDeletedEqualTo((byte) 0).andGeneralIdEqualTo(projectId);
+        List<DcGeneralFile> generalFiles = dcGeneralFileMapper.selectByExample(dcGeneralFileExample);
+        List<String> fileList = new ArrayList<>();
+        generalFiles.forEach(dcGeneralFile -> {
+            fileList.add("https://www.tjsichuang.cn:1443/api/dichan/category/getPicture?id="+dcGeneralFile.getFileId());
+        });
+        return builder.body(ResponseUtils.getResponseBody(fileList));
+    }
+    @ApiOperation(value = "删除项目概况图", notes = "删除项目概况图")
+    @RequestMapping(value = "/deletedProject", method = RequestMethod.POST)
+    public ResponseEntity<JSONObject> deletedProject(Integer fileId) throws Exception {
+        ResponseEntity.BodyBuilder builder = ResponseUtils.getBodyBuilder(HttpStatus.OK);
+        DcFileDesc fileDesc = dcFileDescMapper.selectByPrimaryKey(fileId);
+        FileMangeService fileMangeService = new FileMangeService();
+        fileMangeService.deleteFile(fileDesc.getGroupName(), fileDesc.getRemoteFilename());
+        dcFileDescMapper.deleteByPrimaryKey(fileId);
+        DcGeneralFileExample dcGeneralFileExample = new DcGeneralFileExample();
+        dcGeneralFileExample.createCriteria().andFileIdEqualTo(fileId);
+        return builder.body(ResponseUtils.getResponseBody(dcGeneralFileMapper.deleteByExample(dcGeneralFileExample)));
     }
 }
